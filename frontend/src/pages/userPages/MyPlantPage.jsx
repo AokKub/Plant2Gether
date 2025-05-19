4bb7fd7bc6a0ce8b7f6ef5183b7f81b6465c599c
kykkrm <nonlada.prem@mail.kmutt.ac.th>
2025-05-19 14:27:57 +0700

    create nav bar

---
+++ frontend/src/pages/userPages/MyPlantPage.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../../components/userComponent/userNavBar";
+
+const MyPlantPage = () => {
+	return (
+		<div className="flex flex-col min-h-screen">
+			<NavBar/>
+
+
+			
+		</div>
+	);
+};
+
+export default MyPlantPage;
