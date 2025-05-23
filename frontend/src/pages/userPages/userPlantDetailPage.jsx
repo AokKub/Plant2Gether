fc9f5698f2478e1527a7a4f40608310df22ddaf7
bonusshyn <shayanisshyn@gmail.com>
2025-05-23 16:08:40 +0700

    add footer

--- frontend/src/pages/userPages/userPlantDetailPage.jsx
+++ frontend/src/pages/userPages/userPlantDetailPage.jsx
@@ -1,11 +1,20 @@
-import NavBar from "../../components/userComponent/userNavBar"
-import UserPlantDetailComponent from "../../components/userComponent/userPlantDetailComponent"
+import UserFooterComponent from "../../components/userComponent/userFooter";
+import NavBar from "../../components/userComponent/userNavBar";
+import UserPlantDetailComponent from "../../components/userComponent/userPlantDetailComponent";
 
-export default function UserPlantDetailPage(){
+export default function UserPlantDetailPage() {
   return (
-    <div className="flex flex-col">
-        <NavBar />
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <NavBar />
+
+      <div className="flex-1 md:ml-64"> 
         <UserPlantDetailComponent />
+      </div>
+
+      <div className="md:ml-64">
+        <UserFooterComponent />
+      </div>
     </div>
-  )
+  );
 }
+
